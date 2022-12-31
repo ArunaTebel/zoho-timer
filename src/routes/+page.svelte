77244a045c48df375c8ec7148a4d8e40f0ea4d01
ArunaTebel <arunatebel@hotmail.com>
2022-12-31 14:58:51 +0530

    Revamped the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script>
     import {Portal} from "./util/APIService.js";
     import {onMount} from "svelte";
+    import {PUBLIC_ZOHO_CLIENT_ID, PUBLIC_ZOHO_OAUTH_URL, PUBLIC_ZOHO_SCOPE} from "$env/static/public";
+    import {page} from "$app/stores";
 
     export let data
     let portals = []
+    let oauthUrl = `${PUBLIC_ZOHO_OAUTH_URL}/auth?scope=${PUBLIC_ZOHO_SCOPE}&client_id=${PUBLIC_ZOHO_CLIENT_ID}&response_type=code&access_type=offline&redirect_uri=${$page.url.origin}/auth-callback&prompt=consent`
 
     onMount(fetchPortals)
 
@@ -26,4 +29,14 @@
             </li>
         {/each}
     </ul>
+{:else }
+    <div class="columns">
+        <div class="column is-12 has-text-centered">
+            <a class:is-hidden={data.isAuthorized}
+               class="button is-info is-large mt-6"
+               href="{oauthUrl}">
+                Authorize with ZOHO to start tracking time
+            </a>
+        </div>
+    </div>
 {/if}
